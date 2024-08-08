4b3701fa7f758220463d860821984944f4a0ec87
Lyn-Dah <edulind99@gmail.com>
2024-08-08 13:41:52 +0000

    Add signup-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './Pages/home/home.jsx'
+import SignUp from './Pages/signUp/signUp.jsx'
+import SignIn from './Pages/signIn/signIn.jsx'
+import Profile from './Pages/profile/profile.jsx'
 
-function App() {
-  
 
-  return (
-    <>
-  
-    </>
-  )
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: < Home/>,
+    },
+    {
+      path: '/profile',
+      element: < Profile/>,
+    },
+    {
+      path: '/signUp',
+      element: < SignUp/>,
+    },
+    {
+      path: '/signIn',
+      element: < SignIn/>,
+    },
+    
+  ])
+  return 
 }
 
 export default App
