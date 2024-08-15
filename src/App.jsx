eb5e293ea6d8df7fcb1166f1691a7eaba129a983
FavoriteGirl <jayicon6@gmail.com>
2024-08-15 15:20:16 +0000

    add authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom"
+import Home from "./Pages/Home"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
+import Profile from "./Pages/Profile"
+import ForgotPassword from "./Pages/ForgotPassword/compon/ForgotPassword"
+
 
 
 function App() {
+  const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Home/>
+  }  ,
+  {
+    path: "login",
+    element: <SignIn/>
+  }  ,
+  {
+    path: "signup",
+    element: <SignUp/>
+  } ,
+  {
+    path: "profile",
+    element: <Profile/>
+  },
+  {
+    path: "forgotpassword",
+    element: <ForgotPassword/>
+  }
+  ]
+  )
   
 
   return (
     <>
-  
+  <RouterProvider router={router} />
     </>
   )
 }
